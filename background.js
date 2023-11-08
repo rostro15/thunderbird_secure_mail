c7b23be2ca8debfd1ac18168628ae03609895e6c
rostro15 <teo.rusi@hotmail.fr>
2023-11-08 09:41:29 +0100

    add identification of UL auth

--- background.js
+++ background.js
@@ -1,9 +1,15 @@
 browser.messageDisplay.onMessageDisplayed.addListener((tab, message) => {
     browser.messages.getFull(message.id).then(fullMessage=>{
         console.dir(fullMessage.headers["received"]);
-        reg = RegExp("[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}")
+        browser.messageDisplayAction.setTitle({ title: ( '' )  })
+        reg = /\(Authenticated sender: ([A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4})\)/;
         fullMessage.headers["received"].forEach(element => {
-            console.dir(reg.exec(element));
+            match = element.match(reg);
+            if (match != null) {
+                console.dir(match[1]);
+                browser.messageDisplayAction.setTitle({ title: ( 'from : '+match[1] )  })
+            }
+            console.dir(element.match(reg[1]));
         });
     });
 });
